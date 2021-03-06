be521e1d2d2a054487b997d7a408d11a9c05e957
Aurelien <aurelien.isak@epita.fr>
2021-03-06 21:53:12 +0000

    Add Models

--- pgp_messenger_back/backapp/models.py
+++ pgp_messenger_back/backapp/models.py
@@ -1,4 +1,17 @@
 from django.db import models
-from django.db.models.base import Model
+from typing_extensions import Required
 
 # Create your models here.
+
+class User (models.Model):
+    pgp_public_key = models.CharField(Required=True, max_length=4096)
+    two_factor_auth = models.BooleanField(Required=True)
+    pgp_private_key = models.CharField(Required=False, max_length=4096)
+
+    def __str__(self) -> str:
+        return super().__str__()
+
+class Message (models.Model):
+    user = models.ForeignKey(Required=True)
+    message = models.TextField(Required= True)
+    created_at = models.DateField(auto_now_add=True)
