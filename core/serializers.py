6f59d9403f68971478e3a04b38d28542757ed895
Aurelien Rebourg <aurelien.rebourg@gmail.com>
2021-04-11 19:40:31 +0100

    add password to serializer write only

--- core/serializers.py
+++ core/serializers.py
@@ -3,11 +3,18 @@ from core.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password', 'placeholder': 'Password'}
+    )
+
     class Meta:
         model = User
         fields = (
             'id',
             'username',
+            'password',
             'pgp_public',
             'pgp_private',
             'two_factor_auth',
