0da20b1017b4398da62e85e48a50f9cb12f24a5b
Aurelien Rebourg <aurelien.rebourg@gmail.com>
2021-04-11 19:59:22 +0100

    message: add viewset

--- message/views.py
+++ message/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from message.mixins import ReadWriteSerializerMixin
+from message.models import Conversation
+from message.serializers import (
+    ConversationReadSerializer, ConversationWriteSerializer)
+
+
+class ConversationViewSet(ReadWriteSerializerMixin, ModelViewSet):
+    read_serializer_class = ConversationReadSerializer
+    write_serializer_class = ConversationWriteSerializer
+
+    def get_queryset(self):
+        return Conversation.objects.filter(users=self.request.user)
