bf68ceb481e8e76086b6620812e59717ab4d6f17
Aurelien Rebourg <aurelien.rebourg@gmail.com>
2021-05-04 04:25:12 +0100

    user: hash password before creating user -> fix register

--- core/serializers.py
+++ core/serializers.py
@@ -1,9 +1,17 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 from core.models import User
 
 
+class PasswordField(serializers.CharField):
+    def to_internal_value(self, data):
+        # Hash the password before saving the user model
+        data = make_password(data)
+        return data
+
+
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(
+    password = PasswordField(
         write_only=True,
         required=True,
         style={'input_type': 'password', 'placeholder': 'Password'}
